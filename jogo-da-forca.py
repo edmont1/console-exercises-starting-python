e7459e0d94e3f1b576ea2c3885ab394e9d0af55d
Eduardo <36710107+edmont1@users.noreply.github.com>
2022-08-11 00:50:12 -0300

    Add files via upload

---
+++ jogo-da-forca.py
@@ -0,0 +1,104 @@
+import random
+
+palavras = ['Ampulheta','Anzol','Almofariz','Bidê','Botija','Candelabro','Dedaleira','Desfibrilador','Diapasão','Echarpe','Estribo','Fagote','Fantoche','Fórceps','Freezer','Navalha','Jaleco','Modem','Narguilé','Nebulizador','Novelo','Oboé','Oxímetro','Pêndulo','Quepe','Selim','Sintetizador','Spray','Urinol','Vuvuzela','Webcam','Xadrez','Xequerê','Xilofone','Zíper']
+
+palavra = random.choice(palavras)
+display = []
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+cont = 6
+for l in palavra:
+    display.append('_')
+print('PALAVRA : ', end='')
+while True:
+    print(f"{'  '.join(display)}")
+    guess = input('\nDigite uma letra')
+    #for c in range(0, len(palavra)):
+    #    display.append('_')
+    #    if palavra[c] == guess:
+    #        display[c] = guess
+
+    for i, l in enumerate(palavra):
+        if guess == l:
+            display[i] = guess
+    if '_' not in display or guess == palavra:
+        print(f'A palavra era {palavra}')
+        print('Vc ganhou!!')
+        break
+    else:
+        if guess not in palavra:
+            print(f'A palavra não possui a letra {guess}')
+            print(stages[cont])
+            cont -= 1
+        if cont < 0:
+            print('GAME OVER')
+            break
+
+
+
+
+
+#print(f'\nVc digitou a letra {userguess}')
+
+
+
+
+
+
+
+
